a1941c485ec97536c1644dbb033b1c4d5ae9df49
kyra-jade2 <yz709@cam.ac.uk>
2023-03-08 00:30:26 +0000

    shell for supporting freezed

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name=dev
-#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=8freeze4
+#SBATCH --gres=gpu:8
 
 # run the application
 . /etc/profile.d/modules.sh                                   # Leave this line (enables the module command)
@@ -10,13 +10,16 @@ module purge                                                  # Removes all modu
 source /jmain02/apps/python3/anaconda3/etc/profile.d/conda.sh # enable conda
 conda activate fasterrcnn-env                                 # activate target env
 
-model="FasterRCNN"
+task="freeze4"
+model="Freeze"
+gpu=8
 
 cd /jmain02/home/J2AD015/axf03/yxz79-axf03/fasterrcnn
 python3 run.py \
-    --task_name "dev-"${model} \
+    --task_name ${task}"-"${model}"-gpu-"${gpu} \
     --model ${model} \
     --log_every_n_steps 20 \
-    --num_epochs 100 \
+    --num_epochs 10 \
     --lr 1e-3 \
-    --num_gpu_devices 1
+    --num_gpu_devices ${gpu} \
+    --freeze_depth 8
